b5832cf15e4c22e13006850c65d12d783005d820
kirillmokshin <mygorod11@gmail.com>
2021-09-30 19:57:17 +0300

    Sign in html added js and css edited

--- SwimmingPool_v.1.0/scripts/script.js
+++ SwimmingPool_v.1.0/scripts/script.js
@@ -1,4 +1,15 @@
 function f_log_in(){
-    document.location.replace("https://yandex.ru/");
+    document.location.replace("sign_in.html");
 }
-document.querySelector("#button-start").addEventListener('click', function(){f_log_in()});
+function f_sign_in(){
+    let login = document.querySelector("#login").value;
+    let password = document.querySelector("#password").value;
+    let sys_login = "admin";
+    let sys_password = "admin";
+    if (login == sys_login && password == sys_password){
+        console.log("Right!");
+    }
+    else{
+        console.log("Wrong!");
+    }
+}
