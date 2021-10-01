cae6b874a55cd59db65cb0c7803a3e84573b9b38
Anisia Klimenko <klimenko.an11@gmail.com>
2021-10-01 10:58:01 +0300

    1 - start-training-window with modals, no js, html/css only

--- SwimmingPool_v.1.0/scripts/script.js
+++ SwimmingPool_v.1.0/scripts/script.js
@@ -2,3 +2,11 @@ function f_log_in(){
     document.location.replace("https://yandex.ru/");
 }
 document.querySelector("#button-start").addEventListener('click', function(){f_log_in()});
+
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () { 
+  myInput.focus()
+})
