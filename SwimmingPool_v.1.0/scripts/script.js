bd4ab8eea4bcf934e787eed1b7309ed6f9c039d7
kirillmokshin <mygorod11@gmail.com>
2021-10-06 01:07:51 +0300

    Left card was added to choose sportsmen (style.css, choose_sportsman.html and script.js were changed)

--- SwimmingPool_v.1.0/scripts/script.js
+++ SwimmingPool_v.1.0/scripts/script.js
@@ -1,3 +1,40 @@
+var writers = [{
+    name: "Достоевский Фёдор Михайлович",
+    age: 18,
+    size: 178,
+    weight: 77
+},{
+    name: "Пушкин Александр Сергеевич",
+    age: 19,
+    size: 180,
+    weight: 87
+},{
+    name: "Набоков Владимир Владимирович",
+    age: 38,
+    size: 168,
+    weight: 64
+},{
+    name: "Пелевин Виктор Олегович",
+    age: 23,
+    size: 182,
+    weight: 72
+},{
+    name: "Гоголь Николай Васильевич",
+    age: 34,
+    size: 179,
+    weight: 78
+},{
+    name: "Булгаков Михаил Афанасьевич",
+    age: 29,
+    size: 183,
+    weight: 85
+},{
+    name: "Чехов Антон Павлович",
+    age: 41,
+    size: 188,
+    weight: 89
+}];
+
 function f_log_in(){
     document.location.replace("sign_in.html");
 }
@@ -20,3 +57,37 @@ function f_sign_up(){
 function f_back_to_sign_in(){
     document.location.replace("sign_in.html");
 }
+function f_sportsman_click() {
+    document.addEventListener('click',function(e){
+        for(element of document.getElementsByClassName("list-group")[0].children){
+             element.classList.remove("active");
+         };
+        e.target.classList.add("active");
+        let splitted = e.target.id.split("_");
+        var number = splitted[1];
+        console.log(writers[number-1].name);
+        document.getElementById("s_name").textContent = writers[number-1].name;
+        document.getElementById("s_age").textContent = writers[number-1].age;
+        document.getElementById("s_size").textContent = writers[number-1].size;
+        document.getElementById("s_weight").textContent = writers[number-1].weight;
+    });
+}
+function f_sportsman_search(){
+    let search_text = document.getElementById("search").value;
+    let search_variants = document.querySelectorAll(".list-group li");
+    let names_array = [];
+    for(let v of search_variants){
+        let v_name = v.innerHTML;
+        names_array.push(v_name);
+    }
+    console.log(names_array);
+    for(let i = 0; i < names_array.length; i++){
+        if (names_array[i].includes(search_text) == true){
+            search_variants[i].classList.remove("search-hide");
+        }
+        else{
+            search_variants[i].classList.add("search-hide");
+        }
+    }
+}
+
