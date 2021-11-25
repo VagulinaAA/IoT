2588a2a036cadc5c4cfa6c584da7a8c0b764761b
kirillmokshin <mygorod11@gmail.com>
2021-11-25 22:57:25 +0300

    History was added

--- SwimmingPool_v.1.0/scripts/script.js
+++ SwimmingPool_v.1.0/scripts/script.js
@@ -6,7 +6,19 @@ var writers = [{
     distance: 12,
     tquantity: 3,
     time: 120,
-    avepulse: 74
+    avepulse: 74,
+    train_1: {
+        date: "19/02/2021",
+        pulsemax: 102,
+        pulsemin: 71,
+        distance: 3
+    },
+    train_2: {
+        date: "25/11/2021",
+        pulsemax: 130,
+        pulsemin: 79,
+        distance: 7
+    }
 },{
     name: "Пушкин Александр Сергеевич",
     age: 19,
@@ -15,7 +27,19 @@ var writers = [{
     distance: 12,
     tquantity: 3,
     time: 120,
-    avepulse: 74
+    avepulse: 74,
+    train_1: {
+        date: "19/02/2021",
+        pulsemax: 102,
+        pulsemin: 71,
+        distance: 3
+    },
+    train_2: {
+        date: "25/11/2021",
+        pulsemax: 130,
+        pulsemin: 79,
+        distance: 7
+    }
 },{
     name: "Набоков Владимир Владимирович",
     age: 38,
@@ -24,7 +48,19 @@ var writers = [{
     distance: 12,
     tquantity: 3,
     time: 120,
-    avepulse: 74
+    avepulse: 74,
+    train_1: {
+        date: "19/02/2021",
+        pulsemax: 102,
+        pulsemin: 71,
+        distance: 3
+    },
+    train_2: {
+        date: "25/11/2021",
+        pulsemax: 130,
+        pulsemin: 79,
+        distance: 7
+    }
 },{
     name: "Пелевин Виктор Олегович",
     age: 23,
@@ -33,7 +69,19 @@ var writers = [{
     distance: 12,
     tquantity: 3,
     time: 120,
-    avepulse: 74
+    avepulse: 74,
+    train_1: {
+        date: "19/02/2021",
+        pulsemax: 102,
+        pulsemin: 71,
+        distance: 3
+    },
+    train_2: {
+        date: "25/11/2021",
+        pulsemax: 130,
+        pulsemin: 79,
+        distance: 7
+    }
 },{
     name: "Гоголь Николай Васильевич",
     age: 34,
@@ -42,7 +90,19 @@ var writers = [{
     distance: 12,
     tquantity: 3,
     time: 120,
-    avepulse: 74
+    avepulse: 74,
+    train_1: {
+        date: "19/02/2021",
+        pulsemax: 102,
+        pulsemin: 71,
+        distance: 3
+    },
+    train_2: {
+        date: "25/11/2021",
+        pulsemax: 130,
+        pulsemin: 79,
+        distance: 7
+    }
 },{
     name: "Булгаков Михаил Афанасьевич",
     age: 29,
@@ -51,7 +111,19 @@ var writers = [{
     distance: 12,
     tquantity: 3,
     time: 120,
-    avepulse: 74
+    avepulse: 74,
+    train_1: {
+        date: "19/02/2021",
+        pulsemax: 102,
+        pulsemin: 71,
+        distance: 3
+    },
+    train_2: {
+        date: "25/11/2021",
+        pulsemax: 130,
+        pulsemin: 79,
+        distance: 7
+    }
 },{
     name: "Чехов Антон Павлович",
     age: 41,
@@ -60,7 +132,19 @@ var writers = [{
     distance: 12,
     tquantity: 3,
     time: 120,
-    avepulse: 74
+    avepulse: 74,
+    train_1: {
+        date: "19/02/2021",
+        pulsemax: 102,
+        pulsemin: 71,
+        distance: 3
+    },
+    train_2: {
+        date: "25/11/2021",
+        pulsemax: 130,
+        pulsemin: 79,
+        distance: 7
+    }
 }];
 
 function f_log_in(){
@@ -109,6 +193,24 @@ function f_sportsman_click() {
         localStorage.setItem('objectToPass4', stime);
         var savepulse = writers[number-1].avepulse;
         localStorage.setItem('objectToPass5', savepulse);
+
+        var train_1_date = writers[number-1].train_1.date;
+        localStorage.setItem('objectToPass6', train_1_date);
+        var train_1_pmax = writers[number-1].train_1.pulsemax;
+        localStorage.setItem('objectToPass7', train_1_pmax);
+        var train_1_pmin = writers[number-1].train_1.pulsemin;
+        localStorage.setItem('objectToPass8', train_1_pmin);
+        var train_1_dist = writers[number-1].train_1.distance;
+        localStorage.setItem('objectToPass9', train_1_dist);
+
+        var train_2_date = writers[number-1].train_2.date;
+        localStorage.setItem('objectToPass10', train_2_date);
+        var train_2_pmax = writers[number-1].train_2.pulsemax;
+        localStorage.setItem('objectToPass11', train_2_pmax);
+        var train_2_pmin = writers[number-1].train_2.pulsemin;
+        localStorage.setItem('objectToPass12', train_2_pmin);
+        var train_2_dist = writers[number-1].train_2.distance;
+        localStorage.setItem('objectToPass13', train_2_dist);
     });
 }
 function f_achievements_click(){
@@ -133,6 +235,42 @@ function f_ach_loader(){
     var savepulse = localStorage['objectToPass5'];
     localStorage.removeItem('objectToPass5');
     document.getElementById("s_pulseave").textContent = savepulse;
+}
+function f_history_click(){
+    document.location.replace("history.html");
+}
+function f_history_back_click(){
+    document.location.replace("choose_sportsman.html");
+}
+function f_history_loader(){
+    var sname = localStorage['objectToPass1'];
+    localStorage.removeItem('objectToPass1');
+    document.getElementById("s_name").textContent = sname;
+    var train_1_date = localStorage['objectToPass6'];
+    localStorage.removeItem('objectToPass6');
+    document.getElementById("history_date").textContent = train_1_date;
+    var train_1_pmax = localStorage['objectToPass7'];
+    localStorage.removeItem('objectToPass7');
+    document.getElementById("history_pulse_max").textContent = train_1_pmax;
+    var train_1_pmin = localStorage['objectToPass8'];
+    localStorage.removeItem('objectToPass8');
+    document.getElementById("history_pulse_min").textContent = train_1_pmin;
+    var train_1_dist = localStorage['objectToPass9'];
+    localStorage.removeItem('objectToPass9');
+    document.getElementById("history_distance").textContent = train_1_dist;
+
+    var train_2_date = localStorage['objectToPass10'];
+    localStorage.removeItem('objectToPass10');
+    document.getElementById("history_date_2").textContent = train_2_date;
+    var train_2_pmax = localStorage['objectToPass11'];
+    localStorage.removeItem('objectToPass11');
+    document.getElementById("history_pulse_max_2").textContent = train_2_pmax;
+    var train_2_pmin = localStorage['objectToPass12'];
+    localStorage.removeItem('objectToPass12');
+    document.getElementById("history_pulse_min_2").textContent = train_2_pmin;
+    var train_2_dist = localStorage['objectToPass13'];
+    localStorage.removeItem('objectToPass13');
+    document.getElementById("history_distance_2").textContent = train_2_dist;
 }
 function f_sportsman_search(){
     let search_text = document.getElementById("search").value;
